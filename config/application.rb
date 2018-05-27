9ffe86092f89a21a949b5a41753407f5289edbed
kosuke haruna <haruengineer@gmail.com>
2018-05-28 00:26:39 +0900

    ページネーションの設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Tunagaru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
   end
 end
