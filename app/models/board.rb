3c909a55933afa6fc4ef5954efaa9eeff45c9f48
kosuke haruna <haruengineer@gmail.com>
2018-05-29 00:02:07 +0900

    Commentモデル作成

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,5 @@
 class Board < ApplicationRecord
+  has_many  :comments
   validates :name,  presence: true, length: { maximum: 50 }
   validates :title, presence: true, length: { maximum: 200 }
   validates :body,  presence: true, length: { maximum: 1000}
